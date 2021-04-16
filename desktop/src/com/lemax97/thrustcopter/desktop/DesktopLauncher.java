4d56e9312acaa96a4420f3ba34a48b5a29f238fd
lemax97 <lemax97@mail.ru>
2021-04-16 17:03:01 +0300

    - meteor collision releazed - added pickup - fixed fuel indicator - fixed shield add music - added progress bar - fixed error with Viewport

--- desktop/src/com/lemax97/thrustcopter/desktop/DesktopLauncher.java
+++ desktop/src/com/lemax97/thrustcopter/desktop/DesktopLauncher.java
@@ -4,11 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.lemax97.thrustcopter.ThrustCopter;
 
+import static com.lemax97.thrustcopter.ThrustCopter.screenHeight;
+import static com.lemax97.thrustcopter.ThrustCopter.screenWidth;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
-		config.height = 480;
+		config.width = screenWidth;
+		config.height = screenHeight;
 		new LwjglApplication(new ThrustCopter(), config);
 	}
 }
